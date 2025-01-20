cde6b7d4ef8291d5cd595590ea68c4af09b8bd55
Syed Abdal <syedahmad123123shah@gmail.com>
2025-01-20 12:53:48 +0500

    Initial Commit

---
+++ bottom_nav_bar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bottom_nav_bar
+//
+//  Created by Abdal shah on 20/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bottom_nav_bar/bottom_nav_barApp.swift
@@ -0,0 +1,17 @@
+//
+//  bottom_nav_barApp.swift
+//  bottom_nav_bar
+//
+//  Created by Abdal shah on 20/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct bottom_nav_barApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
