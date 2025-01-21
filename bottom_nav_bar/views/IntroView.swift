9baaf5fe79e87fb87462f7fde579a7993e37e586
Syed Abdal <syedahmad123123shah@gmail.com>
2025-01-21 11:04:14 +0500

    stuff

--- bottom_nav_bar/views/IntroView.swift
+++ bottom_nav_bar/views/IntroView.swift
@@ -13,11 +13,28 @@ struct IntroView: View {
     var body: some View {
         ZStack (alignment: .top) {
             Image(.mountain).resizable().ignoresSafeArea()
+            VStack {
+                Text("Aspen")
+                    .font(.system(size: 56 , design: .monospaced)).italic()
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer()
+                HStack {
+                    Text("Plan your ")
+                        .font(.system(size: 22 , design: .monospaced))
+                        .foregroundColor(.white).multilineTextAlignment(.leading)
+                      
+                }.frame(maxWidth: .infinity)
+              
+                Button {
+                    
+                } label: {
+                    Text("Explore").padding(.vertical, 3).frame(maxWidth: 150)
+                }.buttonStyle(.borderedProminent).padding(.horizontal)
+
+                
+            }
             
-            Text("Aspen")
-                .font(.system(size: 56 , design: .monospaced)).italic()
-                .foregroundColor(.white)
-                .padding()
             
         }
     }
